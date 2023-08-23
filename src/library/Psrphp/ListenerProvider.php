a196bf3f32bba9f2b43eb808cab52f6fbffbefd8
John Doe <johndoe@example.com>
2023-08-23 11:28:50 +0800

    1

--- src/library/Psrphp/ListenerProvider.php
+++ src/library/Psrphp/ListenerProvider.php
@@ -6,32 +6,43 @@ namespace App\Psrphp\Ad\Psrphp;
 
 use App\Psrphp\Ad\Http\Billboard\Index;
 use App\Psrphp\Admin\Model\MenuProvider;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use PsrPHP\Framework\Framework;
 use PsrPHP\Psr11\Container;
 use PsrPHP\Template\Template;
-use PsrPHP\Framework\Listener;
 
-class ListenerProvider extends Listener
+class ListenerProvider implements ListenerProviderInterface
 {
-
-    public function __construct()
+    public function getListenersForEvent(object $event): iterable
     {
-        $this->add(Container::class, function (
-            Container $container
-        ) {
-            $container->set(Template::class, function (
-                Template $template
-            ) {
-                $template->extend('/\{ad\s*([a-zA-Z0-9_]+)\s*\}/Ui', function ($matchs) {
-                    return '<?php echo \App\Psrphp\Ad\Model\Ad::render(\'' . $matchs[1] . '\') ?>';
-                });
-                return $template;
-            });
-        });
-
-        $this->add(MenuProvider::class, function (
-            MenuProvider $provider
-        ) {
-            $provider->add('广告管理', Index::class);
-        });
+        if (is_a($event, Container::class)) {
+            yield function () use ($event) {
+                Framework::execute(function (
+                    Container $container
+                ) {
+                    $container->set(Template::class, function (
+                        Template $template
+                    ) {
+                        $template->extend('/\{ad\s*([a-zA-Z0-9_]+)\s*\}/Ui', function ($matchs) {
+                            return '<?php echo \App\Psrphp\Ad\Model\Ad::render(\'' . $matchs[1] . '\') ?>';
+                        });
+                        return $template;
+                    });
+                }, [
+                    Container::class => $event,
+                ]);
+            };
+        }
+        if (is_a($event, MenuProvider::class)) {
+            yield function () use ($event) {
+                Framework::execute(function (
+                    MenuProvider $provider
+                ) {
+                    $provider->add('广告管理', Index::class);
+                }, [
+                    MenuProvider::class => $event,
+                ]);
+            };
+        }
     }
 }
